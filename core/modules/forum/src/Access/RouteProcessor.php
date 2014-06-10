15b7bce7e8bb29fb5cb6dd0fedbbcf22159ee3eb
Andy Postnikov <apostnikov@gmail.com>
2014-06-11 00:52:19 +0400

    p42

--- core/modules/forum/src/Access/RouteProcessor.php
+++ core/modules/forum/src/Access/RouteProcessor.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\forum\Access;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\RouteProcessor\OutboundRouteProcessorInterface;
 use Drupal\taxonomy\Entity\Term;
 use Symfony\Component\Routing\Route;
@@ -17,13 +18,31 @@ use Symfony\Component\Routing\Route;
 class RouteProcessor implements OutboundRouteProcessorInterface {
 
   /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs a RouteProcessor object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function processOutbound(Route $route, array &$parameters) {
-    if ($route->getPath() == '/taxonomy/term/{taxonomy_term}' && isset($parameters['taxonomy_term'])) {
+    // @todo Check the route machine name here https://drupal.org/node/2283851
+    if ($route->getPath() == '/taxonomy/term/{taxonomy_term}' && !empty($parameters['taxonomy_term'])) {
       // Take over URI construction for taxonomy terms that are forums.
-      if ($vid = \Drupal::config('forum.settings')->get('vocabulary')) {
-        if (Term::load($parameters['taxonomy_term'])->bundle() == $vid) {
+      if ($vid = $this->configFactory->get('forum.settings')->get('vocabulary')) {
+        if (Term::load($parameters['taxonomy_term'])->getVocabularyId() == $vid) {
           $route->setPath('/forum/{taxonomy_term}');
         }
       }
